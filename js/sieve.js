9ad33759a3df32c3fbfea73a41a6cadd43e8bb44
Joseph Lee <monleezy@cat.pdx.edu>
2013-02-18 14:38:00 -0800

    Completed parallel implementation
    Messy...
    
    Signed-off-by: Joseph Lee <monleezy@cat.pdx.edu>

--- js/sieve.js
+++ js/sieve.js
@@ -43,24 +43,30 @@ window.clog = function(msg) { return console.log(msg); };
                 return !n.get('marked');
             });
         },
-        sieve: function() {
-            if (typeof this.point === 'undefined') {
-                this.point = this.nextPrime();
-                this.point.set('prime', 'true');
+        sieve: function(p, row) {
+            var target = this.at(row);
+            //console.log(target);
+            if (target.get('value') % p.get('value') === 0) {
+                target.set('marked', true);
             }
-            if (typeof this.markNext(this.point.get('value')) === 'undefined') {
-                this.point = this.nextPrime();
-                if (typeof this.point === 'undefined') {
-                    console.log('Done: ' + (((new Date()).getTime() - app.start) / 1000) + ' sec');
-                    clearInterval(app.timer);
-                    return undefined;
-                }
-                else {
-                    this.point.set('prime', 'true');
-                }
-                var marked = this.markNext(this.point);
-                //this.remove(marked);
-            }
+            // old ----
+            // if (typeof this.point === 'undefined') {
+            //     this.point = this.nextPrime();
+            //     this.point.set('prime', 'true');
+            // }
+            // if (typeof this.markNext(this.point.get('value')) === 'undefined') {
+            //     this.point = this.nextPrime();
+            //     if (typeof this.point === 'undefined') {
+            //         console.log('Done: ' + (((new Date()).getTime() - app.start) / 1000) + ' sec');
+            //         clearInterval(app.timer);
+            //         return undefined;
+            //     }
+            //     else {
+            //         this.point.set('prime', 'true');
+            //     }
+            //     var marked = this.markNext(this.point);
+            //     //this.remove(marked);
+            // }
         }
     });
 
@@ -102,24 +108,37 @@ window.clog = function(msg) { return console.log(msg); };
         className: 'num',
         id: 'numbers',
         columns: 32,
+        rows: 16,
+        primes: new Numbers(),
         initialize: function() {
-            var max = Math.pow(2, 10) + 1;
-            app.numlist = getRange(3, max, 2);
-            var nums = app.numlist.length;
-            this.rows = Math.ceil(nums / this.columns);
+            this.max = Math.pow(2, 10) + 1;  // 1025
+            // app.numlist = getRange(3, max, 2);
 
-            var i = 0, rowCount, curRow,
-                rowElems = [];      // array of prerendered row dom els
+            //var nums = this.columns * 16;
+            //this.rows = Math.ceil(nums / this.columns);
+
+            this.rowCounter = 0;
+
+            this.initProcessors();
+            this.pi = 0;
 
             //pre-render rows
+            var rowElems = [];      // array of prerendered row dom els
             for (var i = 0; i < this.rows; i++) {
                 rowElems.push($('<tr id="row" class="num"></tr>'));
             }
 
             // add cells vertically
-            for (var i = 0; i < nums; i++) {
-                var newNumView = new NumView(app.numlist.at(i));
-                rowElems[i % this.rows].append(newNumView.el);
+            //for (var i = 0; i < nums; i++) {
+            //    var newNumView = new NumView(app.numlist.at(i));
+            //    rowElems[i % this.rows].append(newNumView.el);
+            //}
+
+            for (var i = 0; i < this.columns; i++) {
+                for (var j = 0; j < this.processors[i].length; j++) {
+                    var newNumView = new NumView(this.processors[i].at(j));
+                    rowElems[j].append(newNumView.el);
+                }
             }
 
             // add all rows to dom
@@ -127,11 +146,64 @@ window.clog = function(msg) { return console.log(msg); };
                 this.$el.append(rowElems[i]);
             }
 
-            if (typeof curRow !== 'undefined') {
-                this.$el.append(curRow);
-            }
-
             return this;
+        },
+        initProcessors: function() {
+            this.processors = [];   // collection of collections
+            var step = 2;
+            for (var i = 0; i < this.columns; i++) {
+                var min = i * this.rows * step + 3,
+                    max = (i + 1) * this.rows * step + 1;
+                this.processors.push(getRange(min, max, step));
+            }
+        },
+        getLength: function() {
+            var len = 0;
+            for (var i = 0; i < this.columns; i++) {
+                len += this.processors[i].length;
+            }
+            return len;
+        },
+        parallelSieve: function() {
+            // perform parallel sieve for each processor
+            var nextPrime;
+            if (this.rowCounter === 0) {
+                nextPrime = this.getNextPrime();
+                nextPrime.set('prime', true);
+                this.primes.add(nextPrime);
+            }
+            for (var i = 0; i < this.processors.length; i++) {
+                this.processors[i].sieve(
+                    this.primes.at(this.primes.length - 1), this.rowCounter);
+            }
+            //this.pi++;
+            if (++this.rowCounter == this.rows) {
+                this.rowCounter = 0;
+            }
+        },
+        getNextPrime: function() {
+            // search thru processors for next prime
+            var nextPrime, counter = 0;
+            while (typeof nextPrime === 'undefined' &&
+                counter < this.getLength()) {
+                for (var col = 0; col < this.columns &&
+                    typeof nextPrime === 'undefined'; col++) {
+                    nextPrime = this.processors[col].nextPrime();
+                }
+                counter++;
+            }
+            return nextPrime;
+        },
+        remainingPrime: function() {
+            // all unmarked numbers are now prime
+            for (var i = 0; i < this.processors.length; i++) {
+                var unmarked = this.processors[i].where({ marked: false });
+                for (var j = 0; j < unmarked.length; j++) {
+                    unmarked[j].set('prime', true);
+                    unmarked[j].set('marked', true);
+                    this.primes.add(unmarked[j]);
+                }
+            }
         }
     });
 
@@ -161,8 +233,15 @@ window.clog = function(msg) { return console.log(msg); };
     app.timer = setInterval(function() {
         if (!app.locked) {
             app.locked = true;
-            app.numlist.sieve();
+            app.table.parallelSieve();
             app.locked = false;
+            var lastPrime = app.table.primes.at(app.table.primes.length - 1);
+            if (Math.pow(lastPrime.get('value'), 2)
+                > app.table.max) {
+                // done sieving. stop timer, mark the rest as prime
+                clearInterval(app.timer);
+                app.table.remainingPrime();
+            }
         }
         else {
             console.log('Wait');
